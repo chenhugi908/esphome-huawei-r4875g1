3feb2528f05325421dca0ca7039b571730b34af9
chenhugi908 <18116094358@189.cn>
2025-06-19 15:53:25 +0800

    can 协议文档

--- components/huawei_r4875g1/sensor.py
+++
@@ -1,8 +0,0 @@
-from . import huawei_sensor_to_code, SENSOR_SCHEMA
-
-import esphome.components.sensor as sensor
-
-CONFIG_SCHEMA = SENSOR_SCHEMA
-
-async def to_code(config):
-    return await huawei_sensor_to_code(config, config[CONF_ID], None, None)
